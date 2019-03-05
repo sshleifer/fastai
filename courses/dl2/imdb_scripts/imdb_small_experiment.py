dca8121ea99492949b9cd6a6046b8eb1a8ce99d5
sshleifer <sam@kensho.com>
2019-03-04 21:01:11 -0800

    try to move to script

---
+++ courses/dl2/imdb_scripts/imdb_small_experiment.py
@@ -0,0 +1,62 @@
+from fastai.text import *
+import html
+import fire
+
+from imdb_scripts.create_toks import make_csv_from_dir, copy_subset_of_files, create_toks
+
+from imdb_scripts.train_clas import train_clas
+from imdb_scripts.eval_clas import eval_clas
+from imdb_scripts.tok2id import tok2id
+from imdb_scripts.finetune_lm import train_lm
+
+
+target_language = 'es'
+WT103_PATH = Path('/home/paperspace/fastai-fork/courses/dl2/wt103/')
+assert WT103_PATH.exists()
+orig_small_data_dir = Path('/home/paperspace/text-augmentation/imdb_small')
+
+
+#big_data_dir = Path('/home/paperspace/text-augmentation/imdb')
+
+def run_experiment(target_language):
+    small_data_dir = Path(f'/home/paperspace/text-augmentation/imdb_small_aug_{target_language}')
+    shutil.copytree(orig_small_data_dir, small_data_dir)
+
+    add_aug_files(target_language, small_data_dir)
+    prepare_tokens_and_labels(small_data_dir)
+    # Finetune LM
+    train_lm(small_data_dir, WT103_PATH, early_stopping=True, cl=10)
+    # Train Classifier
+    learn = train_clas(small_data_dir, 0, bs=64, cl=20)
+    print(eval_clas(small_data_dir))
+    return learn
+    # eval_clas(small_data_dir, val_dir=Path('/home/paperspace/baseline_data/tmp/'))  # CudaError
+
+
+def add_aug_files(target_language, small_data_dir):
+    aug_dir = Path(f'/home/paperspace/text-augmentation/imdb_{target_language}/')
+    selected_train = list((small_data_dir / 'train/').glob('*/*.txt'))
+    assert aug_dir.exists()
+    for p in selected_train:
+        ext = p.relative_to(small_data_dir)
+        p2 = aug_dir.joinpath(ext)
+        if p2.exists():
+            dest_path = str(p)[:-4] + f'_{target_language}.txt'
+            shutil.copy(p2, dest_path)
+
+
+def prepare_tokens_and_labels(small_data_dir):
+    train_csv_path = small_data_dir/'train.csv'
+    make_csv_from_dir(small_data_dir/'train', train_csv_path)
+    trdf = pd.read_csv(small_data_dir/'train.csv', header=None)
+    print(f'train_df.shape: {trdf.shape}')
+    # Copy small val data
+    make_csv_from_dir(small_data_dir/'test', small_data_dir/'val.csv')
+    create_toks(small_data_dir)
+    tok2id(small_data_dir, max_vocab=60000)  # usually don't use this much
+
+
+
+if __name__ == '__main__': fire.Fire(run_experiment)
+
+
