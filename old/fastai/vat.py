57b97f047048ce54a9b7906245a7dc2f977e9f0b
sshleifer <sam@kensho.com>
2019-03-09 22:40:19 -0800

    boom boom

--- old/fastai/vat.py
+++ old/fastai/vat.py
@@ -77,6 +77,7 @@ class VATLoss(nn.Module):
                     logp_hat = self.seq_rnn_emb2logits(model, embedded, attack)
                     mask = torch.zeros_like(logp_hat)
                     mask[-1] = 1
+                    raise ValueError('mask shape: f{mask.shape}')
                     logp_hat *= mask
                     assert not attack.volatile, 'attack volatile before adv_dist.backward()'
                     adv_distance = F.kl_div(logp_hat, pred,)  # EOS Weights?
