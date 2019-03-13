176aefe7e7cc96f067e2d75b68b073435fb35d74
sshleifer <sam@kensho.com>
2019-03-13 10:57:08 -0700

    boom boom

--- old/fastai/lm_rnn.py
+++ old/fastai/lm_rnn.py
@@ -185,30 +185,6 @@ def min_pool(x, shape): return -F.adaptive_max_pool1d(-x, shape)
 
 
 
-
-import torch
-from torch import nn
-import torch.functional as F
-
-class Highway(nn.Module):
-
-    def __init__(self, e_word):
-        super().__init__()
-        self.proj = nn.Linear(e_word, e_word, bias=True)
-        self.gate = nn.Linear(e_word, e_word, bias=True)
-        self.dropout = nn.Dropout(.5)
-        self.sigmoid = torch.nn.Sigmoid()
-
-    def forward(self, xconv_out):
-        """Excpects inputs like (batch_size, e_word), outputs of same shape"""
-        xproj = nn.ReLU()(self.proj(xconv_out))
-        xgate = self.sigmoid(self.gate(xconv_out))
-        xhighway = (xgate * xproj) + ((1 - xgate) * xconv_out)
-        xword_emb = self.dropout(xhighway)
-        return xword_emb
-
-
-
 class PoolingLinearClassifier(nn.Module):
     def __init__(self, layers, drops):
         super().__init__()
