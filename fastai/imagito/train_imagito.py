2956a8493c2f071d17de3b284ee86ad8d692a6d5
sshleifer <sshleifer@gmail.com>
2019-05-13 14:57:45 -0400

    import xresnet50

--- fastai/imagito/train_imagito.py
+++ fastai/imagito/train_imagito.py
@@ -1,5 +1,6 @@
 from fastai.script import *
 from fastai.vision import *
+from fastai.vision.models import xresnet50
 from fastai.callbacks import *
 from fastai.distributed import *
 from fastprogress import fastprogress
@@ -105,7 +106,7 @@ def main(
     if not gpu: print(f'lr: {lr}; eff_lr: {lr*bs_rat}; size: {size}; alpha: {alpha}; mom: {mom}; eps: {eps}')
     lr *= bs_rat
 
-    m = globals()[arch]
+    m = xresnet50# globals()[arch]
     # save params to file like experiments/2019-05-12_22:10/params.pkl
     now = get_date_str(seconds=False)
     Path('experiments').mkdir(exist_ok=True)
