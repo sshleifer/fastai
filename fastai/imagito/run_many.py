871eb009aa2e023027cbafb514d31737f80561c1
sshleifer <sshleifer@gmail.com>
2019-05-13 19:04:37 +0000

    experiments work

--- fastai/imagito/run_many.py
+++ fastai/imagito/run_many.py
@@ -1,12 +1,13 @@
 from sklearn.model_selection import ParameterGrid
-from .train_imagito import main
-from .utils import tqdm_nice
+from fastai.imagito.train_imagito import main
+from fastai.imagito.utils import tqdm_nice
 
 pg = list(ParameterGrid({
         #'lr': lr,
         'size': [32, 64, 128, 256],
         'sample': [1., .5, .25],
         'classes': [None],
+        'fp16': [True],
 }))
 
 
