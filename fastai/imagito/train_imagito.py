54f08c0feeacb76bb4779d624f7b8f2c015b0e09
sshleifer <sshleifer@gmail.com>
2019-05-13 13:53:19 -0700

    dont change original

--- fastai/imagito/train_imagito.py
+++ fastai/imagito/train_imagito.py
@@ -1,6 +1,6 @@
 from fastai.script import *
 from fastai.vision import *
-from fastai.vision.models import xresnet50
+from fastai.vision.models.xresnet2 import XResNet
 from fastai.callbacks import *
 from fastai.distributed import *
 from fastprogress import fastprogress
@@ -63,12 +63,6 @@ def params_to_dict(gpu, woof, lr, size, alpha, mom, eps, epochs, bs, mixup, opt,
         'classes': classes
     }
 
-def results_path(filename):
-    return './results/' + filename
-
-
-#from fastprogress
-
 
 @call_parse
 def main(
@@ -83,11 +77,12 @@ def main(
         bs: Param("Batch size", int)=256,
         mixup: Param("Mixup", float)=0.,
         opt: Param("Optimizer (adam,rms,sgd)", str)='adam',
-        arch: Param("Architecture (xresnet34, xresnet50, presnet34, presnet50)", str)='xresnet50',
+        arch: Param("Architecture (xresnet34, xresnet50, presnet34, presnet50, None)", str)=None,
         dump: Param("Print model; don't train", int)=0,
         fp16=False,
         sample: Param("Percentage of dataset to sample, ex: 0.1", float)=1.0,
-        classes: Param("Comma-separated list of class indices to filter by, ex: 0,5,9", str)=None
+        classes: Param("Comma-separated list of class indices to filter by, ex: 0,5,9", str)=None,
+        save=False,
         ):
     "Distributed training of Imagenette."
     params_dict = locals()
@@ -96,7 +91,6 @@ def main(
     if   opt=='adam' : opt_func = partial(optim.Adam, betas=(mom,alpha), eps=eps)
     elif opt=='rms'  : opt_func = partial(optim.RMSprop, alpha=alpha, eps=eps)
     elif opt=='sgd'  : opt_func = partial(optim.SGD, momentum=mom)
-
     if classes is not None: classes = [int(i) for i in classes.split(',')]
 
     data = get_data(size, woof, bs, sample, classes)
@@ -104,9 +98,11 @@ def main(
     if gpu is not None: bs_rat *= num_distrib()
     if not gpu: print(f'lr: {lr}; eff_lr: {lr*bs_rat}; size: {size}; alpha: {alpha}; mom: {mom}; eps: {eps}')
     lr *= bs_rat
-    m = xresnet50# globals()[arch]
+    m = xresnet50 if arch is None else globals()[arch]
+    # NOTE(SS): globals()[arch] raised KeyError
+
     # save params to file like experiments/2019-05-12_22:10/params.pkl
-    now = get_date_str(seconds=False)
+    now = get_date_str(seconds=True)
     Path('experiments').mkdir(exist_ok=True)
     model_dir = Path(f'experiments/{now}')
     model_dir.mkdir(exist_ok=False)
@@ -126,9 +122,11 @@ def main(
     elif num_distrib()>1: learn.to_distributed(gpu) # Requires `-m fastai.launch`
 
     # save results to a file like 2019-05-12_22:10/metrics.csv
-    csv_logger = CSVLogger(learn, filename=model_dir/'metrics')
-
+    # (CSVLogger model_path/filename + .csv)
+    csv_logger = CSVLogger(learn, filename='metrics')
     learn.fit_one_cycle(epochs, lr, div_factor=10, pct_start=0.3,
                         callbacks=[csv_logger])
+    if save:
+        learn.save('final_classif')
     learn.destroy()
 
