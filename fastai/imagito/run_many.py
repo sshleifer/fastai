ad1e333a61f34d62857cfc1d11c1a372bf41842c
sshleifer <sshleifer@gmail.com>
2019-05-14 12:26:59 -0700

    add try-except to run_many

--- fastai/imagito/run_many.py
+++ fastai/imagito/run_many.py
@@ -2,15 +2,16 @@ from sklearn.model_selection import ParameterGrid
 from fastai.imagito.train_imagito import main
 from fastai.imagito.utils import tqdm_nice, update_batch_size
 
+
 pg = update_batch_size(ParameterGrid({
     # 'lr': lr,
     'bs': [256],
-    'size': [32, 64, 128, 256],
+    'size': [128],
     'sample': [1., .5, .25],
     'classes': [None, [0,1,2,3,4]],
     'fp16': [True],
     'epochs': [20,]
-}))
+}))[11:]
 
 pg2 = update_batch_size(ParameterGrid({
     # 'lr': lr,
@@ -22,10 +23,14 @@ pg2 = update_batch_size(ParameterGrid({
     'epochs': [10,]
 }))
 def run_many(pg):
-
+    failures = []
     for pars in tqdm_nice(pg):
-        main(**pars)
-
+        try:
+            main(**pars)
+        except Exception as e:
+            failures.append(pars)
+            print(e)
+    print(f'failures: {failures}')
 
 if __name__ == '__main__':
     run_many(pg)
