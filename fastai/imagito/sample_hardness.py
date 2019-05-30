4e53eb5299336f9b53a227daf60a7274902d3a84
sshleifer <sshleifer@gmail.com>
2019-05-30 14:54:20 -0700

    Fix bug

--- fastai/imagito/sample_hardness.py
+++ fastai/imagito/sample_hardness.py
@@ -118,15 +118,16 @@ import pandas as pd
 IMAGENETTE_SIZE = 12894
 
 
-def make_hardness_filter_func(hardness_bounds):
+def make_hardness_filter_func(hardness_lower_bound, hardness_upper_bound):
     """
     > hardness_bounds = (0., .25)  # top 25% hardest
     > hardness_bounds = (0., 1.) # all
     > hardness_bounds = (.75, 1.)  # top 25 % easiest
     """
     pred_df = pd.read_msgpack('pred_df.mp').sort_values('loss', ascending=False)
-    start_idx, end_idx = int(IMAGENETTE_SIZE * hardness_bounds[0]), int(
-        IMAGENETTE_SIZE * hardness_bounds[1]) - 1
+    hardness_lower_bound, hardness_upper_bound
+    start_idx, end_idx = int(IMAGENETTE_SIZE * hardness_lower_bound), int(
+        IMAGENETTE_SIZE * hardness_upper_bound) - 1
     pred_df['path'] = pred_df['paths'].apply(os.path.basename)
     all_paths = set(pred_df['path'].unique())
     paths_to_keep = pred_df.iloc[start_idx: end_idx].path.unique()
