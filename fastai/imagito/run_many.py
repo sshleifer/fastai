ddfa4fbec0b3ae82b9caf3c47705a4a090264f63
Eric Prokop <eprokop@splunk.com>
2019-05-30 10:48:37 -0700

    hardness params

--- fastai/imagito/run_many.py
+++ fastai/imagito/run_many.py
@@ -14,26 +14,36 @@ from fastai.imagito.send_sms import try_send_sms
 #     'epochs': [20,]
 # }))[11:]
 
-pg = update_batch_size(ParameterGrid({
-    # 'lr': lr,
-    #'lr': [1e-4, 1e-3, 3e-3, 1e-2, .05, 1e-1],
+# pg = update_batch_size(ParameterGrid({
+#     # 'lr': lr,
+#     #'lr': [1e-4, 1e-3, 3e-3, 1e-2, .05, 1e-1],
+#     'label_smoothing': [True, False],
+#     'size': [128],
+#     'bs': [256],
+#     'sample': [1., .5, .1],
+#     'classes': [None, [0,1,2,3,4], [0,1]],
+# }))
+#
+# pg2 = update_batch_size(ParameterGrid({
+#     # 'lr': lr,
+#     'arch': ['xresnet34', 'xresnet50', 'presnet34', 'presnet50'],
+#     'size': [128],
+#     'bs': [256],
+#     'sample': [1., .5, .1],
+#     'classes': [None, [0,1,2,3,4], [0,1]],
+# }))
+
+pg_hardness = update_batch_size(ParameterGrid({
+    'lr': [1e-4, 1e-3, 3e-3, 1e-2, .05, 1e-1],
     'label_smoothing': [True, False],
     'size': [128],
     'bs': [256],
-    'sample': [1., .5, .1],
-    'classes': [None, [0,1,2,3,4], [0,1]],
+    'hardness': [.75, .5, .25, .1],  # top 50%, top25%
+    'hardness_type': ['loss'],
+    'hardness_model_dir': ['good_model/']
 }))
 
-pg2 = update_batch_size(ParameterGrid({
-    # 'lr': lr,
-    'arch': ['xresnet34', 'xresnet50', 'presnet34', 'presnet50'],
-    'size': [128],
-    'bs': [256],
-    'sample': [1., .5, .1],
-    'classes': [None, [0,1,2,3,4], [0,1]],
-}))
-
-PGS = [pg, pg2]
+PGS = [pg_hardness]
 def run_many(pg):
     try_send_sms(f'Starting {len(pg)} experiments: Params\n {pg}')
     failures = []
