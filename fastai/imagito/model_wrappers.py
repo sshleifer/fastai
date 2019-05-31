d0c6f29b5df272595fa1f695620dbfa172e060d5
sshleifer <sshleifer@gmail.com>
2019-05-30 17:19:23 -0700

    no rp

--- fastai/imagito/model_wrappers.py
+++ fastai/imagito/model_wrappers.py
@@ -1,6 +1,5 @@
 from torchvision.models import AlexNet
-from dataset_distillation.networks import AlexCifarNet
-
+from torch import nn
 
 def alexnet_wrapper(c_out):
     return AlexNet(n_classes=c_out)
@@ -15,3 +14,34 @@ def alex_cifar_net_wrapper(c_out):
     state = FakeDistillState
     state.num_classes = c_out
     return AlexCifarNet(state)
+
+
+class AlexCifarNet(nn.Module):
+    supported_dims = {32}
+
+    def __init__(self, state):
+        super(AlexCifarNet, self).__init__()
+        assert state.nc == 3
+        self.features = nn.Sequential(
+            nn.Conv2d(state.nc, 64, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+            nn.LocalResponseNorm(4, alpha=0.001 / 9.0, beta=0.75, k=1),
+            nn.Conv2d(64, 64, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(inplace=True),
+            nn.LocalResponseNorm(4, alpha=0.001 / 9.0, beta=0.75, k=1),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(4096, 384),
+            nn.ReLU(inplace=True),
+            nn.Linear(384, 192),
+            nn.ReLU(inplace=True),
+            nn.Linear(192, state.num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), 4096)
+        x = self.classifier(x)
+        return x
