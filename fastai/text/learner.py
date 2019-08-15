5b4f3d49d709d9fefb6ca251e751e9d67d3a155e
sshleifer <sshleifer@gmail.com>
2019-08-15 12:57:29 -0700

    convert_weights for embs broken but better

--- fastai/text/learner.py
+++ fastai/text/learner.py
@@ -27,7 +27,8 @@ _model_meta = {AWD_LSTM: {'hid_name':'emb_sz', 'url':URLs.WT103_FWD, 'url_bwd':U
 
 def convert_weights(wgts:Weights, stoi_wgts:Dict[str,int], itos_new:Collection[str]) -> Weights:
     "Convert the model `wgts` to go with a new vocabulary."
-    dec_bias, enc_wgts = wgts.get('1.decoder.bias', None), wgts['0.encoder.weight']
+    import pdb; pdb.set_trace()
+    dec_bias, enc_wgts = wgts.get('1.decoder.bias', None), wgts['0.module.encoder.weight']
     wgts_m = enc_wgts.mean(0)
     if dec_bias is not None: bias_m = dec_bias.mean(0)
     new_w = enc_wgts.new_zeros((len(itos_new),enc_wgts.size(1))).zero_()
@@ -36,8 +37,8 @@ def convert_weights(wgts:Weights, stoi_wgts:Dict[str,int], itos_new:Collection[s
         r = stoi_wgts[w] if w in stoi_wgts else -1
         new_w[i] = enc_wgts[r] if r>=0 else wgts_m
         if dec_bias is not None: new_b[i] = dec_bias[r] if r>=0 else bias_m
-    wgts['0.encoder.weight'] = new_w
-    if '0.encoder_dp.emb.weight' in wgts: wgts['0.encoder_dp.emb.weight'] = new_w.clone()
+    wgts['0.module.encoder.weight'] = new_w
+    if '0.module.encoder_dp.emb.weight' in wgts: wgts['0.module.encoder_dp.emb.weight'] = new_w.clone()
     wgts['1.decoder.weight'] = new_w.clone()
     if dec_bias is not None: wgts['1.decoder.bias'] = new_b
     return wgts
